3ff8528958bcdbbbe10b46c39c880fc5d63e6d79
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-09-20 15:22:17 +0300

    durak hesaplama yapıldı, uyarı mesajları düzenlenecek

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
-    const [item, setItem] = useState([]);
     const [gDurak, setGdurak] = useState();
     const [bDurak, setBdurak] = useState();
     const [gYön, setGyön] = useState();
-
-    //////////////Fetch data.json
-    useEffect(() => {
-        fetch(`data.json`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then((response) => response.json())
-            .then((data) => setItem(data));
-    }, []);
+    const [item, setItem] = useState("....");
 
 
-    /////////////////////Gidilecek durak function
     function handleChange1(event) {
         setGdurak(event.target.value)
     }
@@ -34,70 +21,71 @@ export default function Form() {
     
     function result() {
         if (gYön =="Kadıköy yönü") {
-            console.log("yes");
-           let sonuç = item.kadıköy_yönü;
-            console.log(sonuç);
+           setItem((bDurak-gDurak)*2);
+        } else if (gYön =="Tavşantepe yönü") {
+            setItem((gDurak-bDurak)*2);
         }
-
     }
-
 
     return (
         <div className="form d-flex align-items-center justify-content-center">
             <div className="form-container">
                 <label for="exampleFormControlSelect1">Gidilecek Yön</label>
                 <select name="gYön" onChange={handleChange3} className="form-control mb-5" id="exampleFormControlSelect1">
+                    <option value="">Lütfen gidilecek yönü seçiniz..</option>
                     <option value="Kadıköy yönü">Kadıköy yönü</option>
                     <option value="Tavşantepe yönü">Tavşantepe yönü</option>
                 </select>
                 <label for="exampleFormControlSelect1">Bulunduğunuz Durak</label>
                 <select name="bDurak" onChange={handleChange2} className="form-control mb-5" id="exampleFormControlSelect1">
-                <option value="1">Kadıköy</option>
-                    <option value="Ayrılık Çeşmesi">Ayrılık Çeşmesi</option>
-                    <option value="Acıbadem">Acıbadem</option>
-                    <option value="Ünalan">Ünalan</option>
-                    <option value="Uzunçayır">Uzunçayır</option>
-                    <option value="Göztepe">Göztepe</option>
-                    <option value="Yenisahra">Yenisahra</option>
-                    <option value="Kozyatağı">Kozyatağı</option>
-                    <option value="Bostancı">Bostancı</option>
-                    <option value="Küçükyalı">Küçükyalı</option>
-                    <option value="Maltepe">Maltepe</option>
-                    <option value="Huzurevi">Huzurevi</option>
-                    <option value="Gülsuyu">Gülsuyu</option>
-                    <option value="Esenkent">Esenkent</option>
-                    <option value="Hastane Adliye">Hastane Adliye</option>
-                    <option value="Soğanlık">Soğanlık</option>
-                    <option value="Kartal">Kartal</option>
-                    <option value="Yakacık">Yakacık</option>
-                    <option value="Pendik">Pendik</option>
-                    <option value="Tavşantepe">Tavşantepe</option>
+                    <option value="">Lütfen bulunduğunuz durağı seçiniz..</option>
+                    <option value="1">Kadıköy</option>
+                    <option value="2">Ayrılık Çeşmesi</option>
+                    <option value="3">Acıbadem</option>
+                    <option value="4">Ünalan</option>
+                    <option value="5">Uzunçayır</option>
+                    <option value="6">Göztepe</option>
+                    <option value="7">Yenisahra</option>
+                    <option value="8">Kozyatağı</option>
+                    <option value="9">Bostancı</option>
+                    <option value="10">Küçükyalı</option>
+                    <option value="11">Maltepe</option>
+                    <option value="12">Huzurevi</option>
+                    <option value="13">Gülsuyu</option>
+                    <option value="14">Esenkent</option>
+                    <option value="15">Hastane Adliye</option>
+                    <option value="16">Soğanlık</option>
+                    <option value="17">Kartal</option>
+                    <option value="18">Yakacık</option>
+                    <option value="19">Pendik</option>
+                    <option value="20">Tavşantepe</option>
                 </select>
                 <label for="exampleFormControlSelect1">Gidilecek Durak</label>
                 <select name='gDurak' onChange={handleChange1} className="form-control mb-5" id="exampleFormControlSelect1">
-                    <option value="Kadıköy">Kadıköy</option>
-                    <option value="Ayrılık Çeşmesi">Ayrılık Çeşmesi</option>
-                    <option value="Acıbadem">Acıbadem</option>
-                    <option value="Ünalan">Ünalan</option>
-                    <option value="Uzunçayır">Uzunçayır</option>
-                    <option value="Göztepe">Göztepe</option>
-                    <option value="Yenisahra">Yenisahra</option>
-                    <option value="Kozyatağı">Kozyatağı</option>
-                    <option value="Bostancı">Bostancı</option>
-                    <option value="Küçükyalı">Küçükyalı</option>
-                    <option value="Maltepe">Maltepe</option>
-                    <option value="Huzurevi">Huzurevi</option>
-                    <option value="Gülsuyu">Gülsuyu</option>
-                    <option value="Esenkent">Esenkent</option>
-                    <option value="Hastane Adliye">Hastane Adliye</option>
-                    <option value="Soğanlık">Soğanlık</option>
-                    <option value="Kartal">Kartal</option>
-                    <option value="Yakacık">Yakacık</option>
-                    <option value="Pendik">Pendik</option>
-                    <option value="Tavşantepe">Tavşantepe</option>
+                    <option value="">Lütfen gitmek istediğiniz durağı seçiniz..</option>
+                    <option value="1">Kadıköy</option>
+                    <option value="2">Ayrılık Çeşmesi</option>
+                    <option value="3">Acıbadem</option>
+                    <option value="4">Ünalan</option>
+                    <option value="5">Uzunçayır</option>
+                    <option value="6">Göztepe</option>
+                    <option value="7">Yenisahra</option>
+                    <option value="8">Kozyatağı</option>
+                    <option value="9">Bostancı</option>
+                    <option value="10">Küçükyalı</option>
+                    <option value="11">Maltepe</option>
+                    <option value="12">Huzurevi</option>
+                    <option value="13">Gülsuyu</option>
+                    <option value="14">Esenkent</option>
+                    <option value="15">Hastane Adliye</option>
+                    <option value="16">Soğanlık</option>
+                    <option value="17">Kartal</option>
+                    <option value="18">Yakacık</option>
+                    <option value="19">Pendik</option>
+                    <option value="20">Tavşantepe</option>
                 </select>
                 <button type="button" onClick={() => result()}>Submit</button>
-                <p id="result" className="text-white ">Ortalama ...... dakika sonra {gDurak} durağına varacaksınız.<br /> İyi yolculuklar dileriz..</p>
+                <p id="result" className="text-white">Ortalama {item} dakika sonra {} durağına varacaksınız.<br /> İyi yolculuklar dileriz..</p>
             </div>
         </div>
     )
